7ad36234d7ca6226891d774a7d6e9049295e5835
Farjana2345 <fa8547830@gmail.com>
2021-04-21 18:54:22 +0200

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9yn9k.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 // console.log(uri)
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World')
 })
 app.use(cors());
 app.use(express.json());
